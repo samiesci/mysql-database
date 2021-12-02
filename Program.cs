3ea0f63e642dbae26af96474e58d32e727326ebd
Sami Esci <samiesci2@gmail.com>
2021-12-02 21:26:20 +0300

    mysql-database

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using MySql;
+using System.Data;
+
+namespace mysql_777
+{
+    class Program
+    {
+        private static MySqlConnection newSQL;
+        static void Main(string[] args)
+        {
+            mysql_baglanti();
+            Console.ReadKey();
+        }
+        static void mysql_baglanti()
+        {
+            newSQL = new MySqlConnection("Server=localhost;Database="database_name";charset=utf8;uid=root");
+            try
+            {
+                newSQL.Open();
+                if (newSQL.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Mysql Database bağlantısı sağlandı.");
+                }
+                newSQL.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex);
+
+
+            }
+            Console.ReadKey();
+        }   
+
+
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,6 @@
+mySql localhost server bağlantı kontrolü.
+-----------------------------------------
+✦XAMPP kullanarak local ağda sql bağlantınızı aktif ettikten sonra projeniz ile local ağ arasındaki data akışını sağlamak ve bilgisayarda hazır bulunan database'i projede aktif kullanabilmek için araçtır.
+✦CMD ekranında "mysql" yazarak bilgisayarda bulunan databaseleri görebilir yeni databaseler oluşturabilirsiniz.
+✦İnstagram: @samiesci1
+✦Databaselerinize erişim varsa "Mysql Database bağlantısı sağlandı" çıktısını alırsınız.
